09869f226f10512d7537c74edd70ac07f107375f
JD Ghuman <jdshguman@gmail.com>
2021-02-26 11:01:39 -0600

    Add linting

---
+++ src/components/Widget/Widget.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Widget.module.scss'
+
+const Widget = ({ logo, name, organization, themeColor }) => {
+  return <div className={styles.container}>Widget</div>
+}
+
+export default Widget
