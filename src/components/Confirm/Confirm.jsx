52a23b4a1b9ce55c1a296eaaf3165cf13f9143a8
JD Ghuman <jdshguman@gmail.com>
2021-03-01 09:21:08 -0600

    Add Confirm component

--- src/components/Confirm/Confirm.jsx
+++ src/components/Confirm/Confirm.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { createContext } from 'react'
+import classNames from 'classnames/bind'
+import { truncateText } from '../Utils/Data'
 import styles from './Confirm.module.scss'
+const cx = classNames.bind(styles)
 
-const Confirmation = () => {
-  return <div>Thank you for donating</div>
+const Confirmation = ({ amountRaised }) => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>Thank you!</p>
+      <p className={styles.text__description}>For your donation of</p>
+      <p className={cx('text', 'text__dollar')}>${amountRaised}</p>
+    </div>
+  )
 }
 
 export default Confirmation
